521d3b43b715abcc7f36ea6673948410d1e59be1
ktsyr1 <46694587+ktsyr1@users.noreply.github.com>
2019-05-27 11:13:12 +0300

    1.1
    1

---
+++ app.js
@@ -0,0 +1,20 @@
+var add = [
+    "Qotaiba","nor","alaa"
+]; 
+var adc = [
+    "Qotaiba 3","noor 3","alaa 3"
+];
+document.getElementById("all")
+  .innerHTML = "name ["+ add+"]";
+function sff(){ 
+  var sf = add.lastIndexOf("nor"/*prompt() */);
+   
+  if (add[sf]){
+    document.getElementById("it")
+    .innerHTML = sf;
+    document.getElementById("iy")
+    .innerHTML = add[sf];
+}  
+   }
+ 
+  
