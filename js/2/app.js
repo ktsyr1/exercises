f8b4bf66be272d18bdb2d92cb56dab8ec6adb9c8
ktsyr1 <46694587+ktsyr1@users.noreply.github.com>
2019-07-19 16:38:41 +0300

    bm

--- js/2/app.js
+++ js/2/app.js
@@ -1,14 +1,24 @@
-var Cfull    =   ( 7.47+10.12+6.3+16.47+10.56+13.45+14.20+7.45+7.08+15.42+8.10); 
- function CfullT(){
-  var v = Cfull  ,
-      v1= Math.floor( v / 60) ,
-       v2 =    v % 60 , 
-       v3 = Math.floor(v % 60)
-       v4 = v3 - v2 ;
-   var v5 = v4.toString().slice(3,5),
-       v6 = v1 +":"+ v3 +":"+ v5 ; 
-       document.write( "باقي لنهاية الكورس  : "+ v6 );
-       alert("باقي لنهاية الكورس  : "+ v6 ); }
-       
- CfullT();
+var Cfull = (
+
+
+    11.24 + 9.58 + 8.44 + 20.39 + 7.27 +
+    8.45 + 17.09 + 7.33 + 8.00 + 5.39 +
+    8.22 + 14.49 + 4.17 + 7.28 + 6.14 +
+    6.37 + 6.01 + 14.03 + 11.27 + 5.39 +
+    10.55 + 12.05 + 4.38 + 2.39 + 2.08
+
+);
+function CfullT() {
+    var v = Cfull,
+        v1 = Math.floor(v / 60),
+        v2 = v % 60,
+        v3 = Math.floor(v % 60)
+    v4 = v3 - v2;
+    var v5 = v4.toString().slice(3, 5),
+        v6 = v1 + "." + v3 + "." + v5;
+    document.write("باقي لنهاية الكورس  . " + v6);
+     
+};
+
+CfullT();
 
