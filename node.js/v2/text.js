b5fc35cc1509d13ced4ddc425568be6cb5718f39
ktsyr1 <46694587+ktsyr1@users.noreply.github.com>
2019-10-19 20:01:24 +0300

    2

---
+++ node.js/v2/text.js
@@ -0,0 +1,35 @@
+
+// ------------------------- error
+
+
+
+// ------------------------- timer
+// setInterval(() => {
+//     console.log("hi")
+// }, 1000)
+// var recursive = function () {
+//     console.log("ktsyr1");
+//     setTimeout(recursive, 1000);
+// }
+//  function ktsyr1() {
+//     console.log("ktsyr1"); 
+// }
+// var kt =setTimeout(ktsyr1,1000),
+//     syr1 =setInterval(ktsyr1,1000);
+//     clearTimeout(kt); //يبقى يعمل
+//     clearInterval(syr1); // يعمل مرة واحدة
+// --------------------------------- OS
+// const os = require('os');
+// console.log(os.freemem());
+// console.log(os.homedir());
+// console.log(os.hostname());
+// console.log(os.endianness());
+// console.log(os.loadavg());
+// console.log(os.platform());
+// console.log(os.release());
+// console.log(os.tmpdir());
+// console.log(os.cpus());
+// console.log(os.type());
+// console.log(os.cpus());
+// console.log(os.arch());
+// console.log(os.networkInterfaces());  
