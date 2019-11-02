319cec008113daff5fbcf2fb77b9b5cf1389fe6c
ktsyr1 <46694587+ktsyr1@users.noreply.github.com>
2019-11-02 05:03:12 +0200

    5

--- mongoDB/app.js
+++ mongoDB/app.js
@@ -1,2 +1,13 @@
- const mdb =require('mongodb').mdb;
- 
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/test';
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  db.collection('mongoclient_test').update({"app":"app mp4","os":"web","size":40})
+  console.log("Connected correctly to server");
+
+  db.close();
+});
