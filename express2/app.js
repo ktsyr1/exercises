b288a4f66ce0683a2aef48a764a730a038f29f8f
ktsyr1 <46694587+ktsyr1@users.noreply.github.com>
2019-11-19 00:04:08 +0200

    .

--- express2/app.js
+++ express2/app.js
@@ -1,6 +1,18 @@
 const express =require('express');
+const cors =require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
 const app = express();
-const router =require('./api/router'); 
-app.use(router)
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.URIX;
+mongoose.connect(uri, {useCreateIndex : true ,useNewUrlParser :true})
+
+const connection = mongoose.connection;
+connection.once('open',()=>console.log('db connect'))
 app.listen(port,()=>console.log(`run server ...${port}`))
