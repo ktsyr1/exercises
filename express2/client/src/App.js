2f4200f44afc58a2166678c4941d998500c8f5f7
ktsyr1 <46694587+ktsyr1@users.noreply.github.com>
2019-11-21 20:56:59 +0200

    مر

--- express2/client/src/App.js
+++ express2/client/src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route} from "react-router-dom";
-import Navbar from "./components/navbar.component"
-import CenterList from "./components/center-list.component";
-import EditCenter from "./components/edit-center.component";
-import CreateCenter from "./components/create-center.component";
-import CreateUser from "./components/create-user.component";
+import Navbar from "./components/navbar"
+import CenterList from "./components/center-list";
+import EditCenter from "./components/edit-center";
+import CreateCenter from "./components/create-center";
+import CreateUser from "./components/create-user";
 
 function App() {
   return (
---
+++ express2/client/src/components/edit-center.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'; 
+export default class EditCenter extends Component {
+
+  render() {
+    return (
+    <div>edit Component</div>
+    );
+  }
+}
